570efd2d566de4833083e2d489b9b7ea428f0839
Riri <jamesriri03@gmail.com>
2024-04-10 23:36:34 +0300

    Add error handling

--- flaskapp/webapp/__init__.py
+++ flaskapp/webapp/__init__.py
@@ -1,4 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template, request, jsonify
+
+def not_found(e):
+    if request.accept_mimetypes.accept_json and not request.accept_mimetypes.accept_html:
+        response = jsonify({
+            "Error": "Not Found"
+        })
+        response.status_code = 404
+    return render_template("errors/404.html"), 404
+
+def forbidden(e):
+    if request.accept_mimetypes.accept_json and not request.accept_mimetypes.accept_html:
+        response = jsonify({
+            "Error": "Forbidden Access"
+        })
+        response.status_code = 403
+    return render_template("errors/403.html"), 403
+
+def internal_server(e):
+    if request.accept_mimetypes.accept_json and not request.accept_mimetypes.accept_html:
+        response = jsonify({
+            "Error": "Internal server error"
+        })
+        response.status_code = 500
+    return render_template("errors/500.html"), 500
+
 
 def create_app(object):
     app = Flask(__name__.split('.')[0])
@@ -9,6 +34,10 @@ def create_app(object):
     
     main_create_module(app)
     auth_create_module(app)
+    
+    app.register_error_handler(403, forbidden)
+    app.register_error_handler(404, not_found)
+    app.register_error_handler(500, internal_server)
     
     return app
     
