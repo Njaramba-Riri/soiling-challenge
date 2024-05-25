1e67a39bad65240cc889e73863616f95d3c903f1
Riri <jamesriri03@gmail.com>
2024-05-26 02:20:07 +0300

    Update dependencies

--- config.py
+++ config.py
@@ -15,8 +15,8 @@ class Config(ABC):
 class DevConfig(Config):
     SECRET_KEY = '\xd0\xb2W\x05\x11\xaf_\xc8\xb7\xdd<\x80\x9cj\x9aTv\x9c\xe6\x0b\xea\x02\x1eS'
     CACHE_TYPE = 'simple'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, "soiling.db") or \
-        os.environ.get('DATABASE_URL') 
+    SQLALCHEMY_DATABASE_URI =  os.environ.get('DATABASE_URL') \
+        or 'sqlite:///' + os.path.join(basedir, "soiling.db")
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     SQLALCHEMY_ECHO = True
     DEBUG_TB_INTERCEPT_REDIRECTS = False
